c0f3cd757442eed324cf01bb0e7d51e49a65401c
rienegoh <rg6868@hotmail.com>
2021-06-26 12:49:45 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Riene Goh on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
